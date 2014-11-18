ca8b8cc944025eccc984fbbd2758829112da9c70
Dilawar Singh <dilawars@ncbs.res.in>
2014-11-18 00:22:34 -0500

    Added class to read aiff file.

---
+++ aiff/aiff.cc
@@ -0,0 +1,69 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  aiff.cpp
+ *
+ *    Description:  Class to open a file.
+ *
+ *        Version:  1.0
+ *        Created:  11/18/2014 12:18:52 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dilawar Singh (), dilawars@ncbs.res.in
+ *   Organization:  
+ *
+ * ==============================================================================
+ */
+
+#include "aiff.h"
+
+/*
+ *-------------------------------------------------------------------------------
+ *       Class:  Aiff
+ *      Method:  Aiff
+ * Description:  constructor
+ *-------------------------------------------------------------------------------
+ */
+Aiff::Aiff ()
+{
+    
+}  /* -----  end of method Aiff::Aiff  (constructor)  ----- */
+
+/*
+ *-------------------------------------------------------------------------------
+ *       Class:  Aiff
+ *      Method:  Aiff
+ * Description:  copy constructor
+ *-------------------------------------------------------------------------------
+ */
+Aiff::Aiff ( const Aiff &other )
+{
+}  /* -----  end of method Aiff::Aiff  (copy constructor)  ----- */
+
+/*
+ *-------------------------------------------------------------------------------
+ *       Class:  Aiff
+ *      Method:  ~Aiff
+ * Description:  destructor
+ *-------------------------------------------------------------------------------
+ */
+Aiff::~Aiff ()
+{
+}  /* -----  end of method Aiff::~Aiff  (destructor)  ----- */
+
+/*
+ *-------------------------------------------------------------------------------
+ *       Class:  Aiff
+ *      Method:  operator =
+ * Description:  assignment operator
+ *-------------------------------------------------------------------------------
+ */
+Aiff& Aiff::operator = ( const Aiff &other )
+{
+    if ( this != &other )
+    {
+    }
+    return *this;
+}  /* -----  end of method Aiff::operator =  (assignment operator)  ----- */
+
---
+++ aiff/aiff.h
@@ -0,0 +1,34 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  aiff.h
+ *
+ *    Description:  Class to read aiff file.
+ *
+ *        Version:  1.0
+ *        Created:  11/18/2014 12:17:57 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dilawar Singh (), dilawars@ncbs.res.in
+ *   Organization:  
+ *
+ * ==============================================================================
+ */
+
+
+#ifndef  AIFF_INC
+#define  AIFF_INC
+
+class Aiff
+{
+    public:
+        Aiff();
+
+        ~Aiff();
+
+    public:
+        void openFile(char* filename);
+}
+
+#endif   /* ----- #ifndef AIFF_INC  ----- */
