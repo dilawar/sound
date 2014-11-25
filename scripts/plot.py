035266fa74813843f2b558b09accc7e3d2e57699
Dilawar Singh <dilawars@ncbs.res.in>
2014-11-25 11:25:38 +0530

    Added script which plot data.

--- scripts/plot.py
+++ scripts/plot.py
@@ -19,9 +19,11 @@ def plot(data):
     pylab.plot(x, y)
     pylab.show()
 
-def plotRow(file, length = -1):
+def plotRow(args):
     data = [[]]
-    for line in file:
+    filename = args.outfile
+    length = args.length
+    for line in args.file:
         row = line.split(',')[1:-1]
         if length <= 0:
             length = len(row) - 1
@@ -29,14 +31,40 @@ def plotRow(file, length = -1):
 
     for d in data:
         pylab.plot(range(len(d)), d)
-    pylab.show()
+    if not filename:
+        pylab.show()
+    else:
+        pylab.savefig(filename)
 
 
 
 def main():
-    filename = sys.argv[1]
-    with open(filename, "r") as f:
-        plotRow(f, 1000)
+    import argparse
+    # Argument parser.
+    description = '''Plot data stored in a file'''
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--file', '-f'
+            , required = True
+            , type = argparse.FileType('r')
+            , help = 'File to plot'
+            )
+    parser.add_argument('--length', '-l'
+            , required = False
+            , default = -1
+            , type = int
+            , help = "No of points to plot"
+            )
+    parser.add_argument('--outfile', '-o'
+            , required = False
+            , default = None
+            , type = argparse.FileType('w')
+            , help = "Output file"
+            )
+
+    class Args: pass 
+    args = Args()
+    parser.parse_args(namespace=args)
+    plotRow(args)
 
 if __name__ == '__main__':
     main()
