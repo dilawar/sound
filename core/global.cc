c5256e3af5b75f6be8025aa87681bfac77c870a6
Dilawar Singh <dilawars@ncbs.res.in>
2014-11-26 12:33:49 +0530

    Added cmakelists and creaated a new library for global.

---
+++ core/global.cc
@@ -0,0 +1,19 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  global.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/26/2014 12:31:35 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dilawar Singh (), dilawars@ncbs.res.in
+ *   Organization:  
+ *
+ * ==============================================================================
+ */
+#include <stdlib.h>
+
